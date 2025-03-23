51b540910be92d50699b5bef09df4f0c6cfd0202
Moatazzenaty <motazzenaty@gmail.com>
2025-03-23 22:46:38 +0200

    Add files via upload
    session 2

---
+++ weather advisor assignment.py
@@ -0,0 +1,9 @@
+temp = int( input ("enter the temp in celsius "))
+if temp >= 30:
+    print("it's hot today. stay hydrated ")
+elif temp < 30 and temp >20 :
+    print("it's warm today . enjoy the weather ")
+elif temp < 20 and temp > 10 :
+    print("it's a cool day. wear a jacket ! ") 
+else :
+    print("it's a cold day . stay warm ")       
